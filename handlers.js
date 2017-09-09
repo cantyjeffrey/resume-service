1fe319dbeaacf8cbae1b304326d9858eb8ef6dab
Jeffrey Canty <hello@jeffrey.engineer>
2017-09-09 00:48:15 -0500

    Add routes.

---
+++ handlers.js
@@ -0,0 +1,51 @@
+const { send, json } = require('micro');
+const puppeteer = require('puppeteer');
+
+const { CHROME_BIN } = process.env;
+
+const main = async function(req, res) {
+  const params = await json(req);
+  const { width, height, url, fullPage } = params;
+  if (!url) {
+    const error = {
+      status: '400',
+      code: 400,
+      title: 'Bad Request',
+      detail: 'Missing `url` in request body.',
+    };
+    send(res, 400, error);
+  }
+  const config = {
+    ignoreHTTPSErrors: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  };
+  if (CHROME_BIN) config.executablePath = CHROME_BIN;
+  const browser = await puppeteer.launch(config);
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: parseInt(width) || 1366,
+    height: parseInt(height) || 1024,
+    deviceScaleFactor: 1,
+  });
+  await page.goto(url, {
+    waitUntil: 'networkidle',
+  });
+  const buffer = await page.screenshot({
+    fullPage: Boolean(fullPage) ? true : false,
+  });
+  const response = {
+    url,
+    params,
+    dataURI: await buffer.toString('base64'),
+  };
+  browser.close();
+  send(res, 200, response);
+};
+
+const index = () => {
+  return 'puppeteer-micro';
+};
+
+const noop = () => {};
+
+module.exports = { index, main };
